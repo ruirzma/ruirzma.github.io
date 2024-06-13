6c2b10fd825b55010f4f805d7c6d16827b43c178
ruirzma <ruimarui@163.com>
2024-06-13 12:00:48 +0800

    v0.0.73

--- assets/js/clustrMaps.js
+++ assets/js/clustrMaps.js
@@ -1,13 +1,1 @@
----
-layout: cv
-permalink: /cv/
-title: cv
-nav: true
-nav_order: 5
-cv_pdf: RuiMa-CV.pdf
-description:
-toc:
-  sidebar: left
----
-
 <script type='text/javascript' id='clustrmaps' src='//cdn.clustrmaps.com/map_v2.js?cl=ffffff&w=a&t=n&d=u4pZWGBli0o9AFqj2iNv5t9riJF6nGwMkKguhNg8z6Q'></script>
